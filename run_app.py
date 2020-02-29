1767e110a2e089527de2d6dbff4fcb658914a079
Manas Kulkarni <manas.msk8067@gmail.com>
2020-02-29 14:06:42 -0500

    Auto stash before merge of "msk" and "origin/staging"

--- run_app.py
+++ run_app.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from com.familytree.stories.UserStoriesNy import UserStoriesNy
 from com.familytree.stories.UserStoriesDg import UserStoriesDg
 from com.familytree.stories.UserStoriesAm import UserStoriesAm
@@ -21,3 +22,18 @@ us1.us07()
 us2 = UserStoriesAm()
 us2.us02()
 us2.us06()
+=======
+from com.familytree.stories.UserStoriesMSK import UserStoriesMSK
+import os
+from os import sys, path
+
+# sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+# print(sys.executable)
+# print(os.getcwd())
+# print(sys.path)
+usmsk = UserStoriesMSK()
+usmsk.print_list('US 09', usmsk.us09())
+# us.us01(r"C:\Users\naman\PycharmProjects\CS-555\com\familytree\data\us01.ged")
+# us.us08(r"C:\Users\naman\PycharmProjects\CS-555\com\familytree\data\us08.ged")
+#us.log()
+>>>>>>> Stashed changes
