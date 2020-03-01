04574e053838bc20b13dc2c36ea5daeed68f7a05
Manas Kulkarni <manas.msk8067@gmail.com>
2020-02-29 20:00:56 -0500

    US09 US10 updated

--- com/familytree/sprints/Sprint.py
+++ com/familytree/sprints/Sprint.py
@@ -1,10 +1,8 @@
 import os
 
 from com.familytree.TreeUtils import TreeUtils
-<<<<<<< Updated upstream
-=======
 from com.familytree.stories.UserStoriesMSK import UserStoriesMSK
->>>>>>> Stashed changes
+
 from com.familytree.stories.UserStoriesNy import UserStoriesNy
 
 
@@ -13,15 +11,13 @@ class Sprint:
     @staticmethod
     def run_sprint1():
         error_list = []
+        fp=os.path.join(os.path.realpath('.'), 'com', 'familytree', 'data', 'Familytree_gedcom_yadav.ged')
         usny = UserStoriesNy()
-<<<<<<< Updated upstream
-=======
         usmsk = UserStoriesMSK()
-        error_list.extend(usmsk.us09())
-        error_list.extend(usmsk.us10())
->>>>>>> Stashed changes
-        error_list.extend(usny.us01(os.path.join(os.path.realpath('.'), 'com', 'familytree', 'data', 'Familytree_gedcom_yadav.ged')))
-        error_list.extend(usny.us08(os.path.join(os.path.realpath('.'), 'com', 'familytree', 'data', 'Familytree_gedcom_yadav.ged')))
+        error_list.extend(usmsk.us09(fp))
+        error_list.extend(usmsk.us10(fp))
+        error_list.extend(usny.us01(fp))
+        error_list.extend(usny.us08(fp))
         TreeUtils.print_report('Sprint 1 Report', error_list)
 
 # if __name__ == '__main__':
--- run_app.py
+++ run_app.py
@@ -1,3 +1,4 @@
+from com.familytree.sprints.Sprint import Sprint
 from com.familytree.stories.UserStoriesNy import UserStoriesNy
 import os
 from os import sys, path
