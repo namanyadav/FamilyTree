7dc9aae3cfbb22dbb68f7062b63937f2efa6e4fe
naman_yadav <namanyadav@gmail.com>
2020-03-02 13:56:13 -0500

    adding corner condition for us05 and us08 adding us05 and us07 gedcom files for divya adding exception handling in Sprint

--- com/familytree/sprints/Sprint.py
+++ com/familytree/sprints/Sprint.py
@@ -13,15 +13,36 @@ class Sprint:
     def run_sprint1():
         error_list = []
         fp = os.path.join(os.path.realpath('.'), 'com', 'familytree', 'data', 'Familytree_gedcom_yadav.ged')
-        tree_line = TreeLine()
-        tree_line.tabulate(tree_line.process_data(fp))
-        usny, usmsk, usam, usdg = UserStoriesNy(), UserStoriesMSK(), UserStoriesAm(), UserStoriesDg()
-        error_list.extend(usmsk.us09(fp))
-        error_list.extend(usmsk.us10(fp))
-        error_list.extend(usny.us01(fp))
-        error_list.extend(usny.us08(fp))
-        error_list.extend(usam.us02(fp))
-        error_list.extend(usam.us06(fp))
-        error_list.extend(usdg.us05(fp))
-        error_list.extend(usdg.us07(fp))
-        TreeUtils.print_report('Sprint 1 Report', error_list)
+        try:
+            tree_line = TreeLine()
+            tree_line.tabulate(tree_line.process_data(fp))
+            usny, usmsk, usam, usdg = UserStoriesNy(), UserStoriesMSK(), UserStoriesAm(), UserStoriesDg()
+            error_list.extend(usmsk.us09(fp))
+            error_list.extend(usmsk.us10(fp))
+            error_list.extend(usny.us01(fp))
+            error_list.extend(usny.us08(fp))
+            error_list.extend(usam.us02(fp))
+            error_list.extend(usam.us06(fp))
+            error_list.extend(usdg.us05(fp))
+            error_list.extend(usdg.us07(fp))
+            TreeUtils.print_report('Sprint 1 Report', error_list)
+        except FileNotFoundError:
+            print(f'File not found: {fp}')
+        except Exception as e:
+            print(f'Exception occurred: {str(e)}')
+
+    @staticmethod
+    def run_sprint_test():
+        error_list = []
+        fp = os.path.join(os.path.realpath('.'), 'com', 'familytree', 'data', 'us05.ged')
+        try:
+            tree_line = TreeLine()
+            tree_line.tabulate(tree_line.process_data(fp))
+            usdg = UserStoriesDg()
+            error_list.extend(usdg.us05(fp))
+            # error_list.extend(usdg.us07(fp))
+            TreeUtils.print_report('USDG', error_list)
+        except FileNotFoundError:
+            print(f'File not found: {fp}')
+        except Exception as e:
+            print(f'Exception occurred: {str(e)}')
