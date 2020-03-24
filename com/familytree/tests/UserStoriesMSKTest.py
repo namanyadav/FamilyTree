4a3b9e1d8f54de2b8897e13a117ae3d38f2b0fd4
Manas Kulkarni <manas.msk8067@gmail.com>
2020-03-23 20:14:24 -0400

    Sprint 2 Manas

--- com/familytree/tests/UserStoriesMSKTest.py
+++ com/familytree/tests/UserStoriesMSKTest.py
@@ -23,6 +23,16 @@ class UserStoriesMSKTest(unittest.TestCase):
         error_list = self.stories.us10(get_data_file_path('us10.ged'))
         self.assertEqual(['F1', 'F2', 'F4'], get_id_list(error_list))
 
+    def test_us17(self):
+        """ I3 have failing criteria in us17.ged """
+        error_list = self.stories.us17(get_data_file_path('us17.ged'))
+        self.assertEqual(['I3'], get_id_list(error_list))
+
+    def test_us18(self):
+        """ I1, I2, I6, I8 have failing criteria in us18.ged """
+        error_list = self.stories.us18(get_data_file_path('us18.ged'))
+        self.assertEqual(['I1', 'I2', 'I6', 'I8'], get_id_list(error_list))
+
 
 if __name__ == "__main__":
     unittest.main()
